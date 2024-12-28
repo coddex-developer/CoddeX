5dde7ed614b10196cceaff2e8daba99cfd1ada05
CoddeX <gabrielrodrigueslima22@gmail.com>
2024-12-28 20:01:16 -0300

    Atualizando informações dos Cards predefinidos

--- src/models/contentPageIndelModel.js
+++ src/models/contentPageIndelModel.js
@@ -2,37 +2,34 @@ const contentIndex = {
   title: `Bem-vindo ao meu Portfólio`,
   subtitle: `Transformando ideias em projetos incríveis!`,
   titleAboutMe: `Sobre Mim`,
-  textAboutMe: `Sou um desenvolvedor apaixonado por tecnologia, com habilidades em front-end e back-end, sempre buscando entregar soluções criativas e eficientes. Amo aprender e ajudar outras pessoas a se desenvolverem na área de tecnologia!`,
+  textAboutMe: `Olá me chamo Gabriel Rodrigues ou um desenvolvedor apaixonado por tecnologia, com habilidades em Front-End e Back-End, sempre buscando entregar soluções criativas e eficientes. Amo aprender e ajudar outras pessoas a se desenvolverem na área de tecnologia!`,
   projects: [
     {
       id: 'c4f726e9-d73a-430b-aedc-0f3de4ac582e',
       imagePj: 'https://i.pinimg.com/originals/75/c2/f8/75c2f842863ae2df6b3ac2d0a4d63026.gif',
-      titlePj: 'Meu Projeto Fictício',
-      descriptionPj: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      titlePj: 'CalcJS',
+      descriptionPj: 'Calculadora de operações simples desenvolvido com HTML, CSS e JAVASCRIPT',
       linkPj: 'https://www.lipsum.com/feed/html'
     },
     {
       id: 'dedb6148-3cdf-44c2-9a2f-10ca7d31ba5c',
       imagePj: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTta_eifGeOny4rrUZyX6GPBdu-IVKHspE0z7qCEdBeef9uR5FLz5KbhDZV&s=10',
-      titlePj: 'Meu Projeto 2° Fictício',
-      descriptionPj: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.\r\n' +
-        'Proin lobortis, ante vitae',
-      linkPj: 'https://www.lipsum.com/feed/html'
+      titlePj: 'Card QRCODE',
+      descriptionPj: 'Desafio do Front-End Mentor desenvolvido com HTML e CSS',
+      linkPj: 'https://coddex-developer.github.io/solucQrcode/'
     },
     {
       id: 'dedb6148-3cdf-44c2-9a2f-10ca7d31ba53c',
       imagePj: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0wMzX1iAp58jPaELEduIoUZ-0VUprb-_khA&usqp=CAU',
-      titlePj: 'Meu Projeto 3° Fictício',
-      descriptionPj: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.\r\n' +
-        'Proin lobortis, ante vitae',
-      linkPj: 'https://www.lipsum.com/feed/html'
+      titlePj: 'Landing Page Blog',
+      descriptionPj: 'Uma landing page de um Blog desenvolvido com HTML CSS e JAVASCRIPT',
+      linkPj: 'https://project-loja-virtual.vercel.app/'
     },
     {
       id: 'dedb6148-3cdf-44c2-9a2f-10ca7d31ba5fhc',
       imagePj: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdTOFGbY_fcvlL1FrlWpOdtqFuSiehuyA50A&usqp=CAU',
       titlePj: 'Meu Projeto 4° Fictício',
-      descriptionPj: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.\r\n' +
-        'Proin lobortis, ante vitae',
+      descriptionPj: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.\r\n',
       linkPj: 'https://www.lipsum.com/feed/html'
     }
   ]
