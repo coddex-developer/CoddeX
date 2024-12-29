a9022018a56466273b7b97c95d22ee8fbec2e83c
CoddeX <gabrielrodrigueslima22@gmail.com>
2024-12-29 13:29:50 -0300

    Ajustandes cards certificados posicionamento

--- public/script/scroll.js
+++ public/script/scroll.js
@@ -5,16 +5,16 @@ export default function scrollFunc() {
   scrollContainer.addEventListener('scroll', () => {
     let center = scrollContainer.scrollLeft + scrollContainer.offsetWidth / 2;
 
-    scrollItems.forEach(item => {
-      const itemLeft = item.offsetLeft;
-      const itemRight = itemLeft + item.offsetWidth;
+      scrollItems.forEach(item => {
+        const itemLeft = item.offsetLeft;
+        const itemRight = itemLeft + item.offsetWidth;
 
-      if (center >= itemLeft && center <= itemRight) {
-        item.style.transform = 'scale(1.1)'; 
-        item.style.transition = 'transform 0.2s ease';
-      } else {
-        item.style.transform = 'scale(1)';
-      }
-    });
+        if (center >= itemLeft && center <= itemRight) {
+          item.style.transform = 'scale(1.1)';
+          item.style.transition = 'transform 0.2s ease';
+        } else {
+          item.style.transform = 'scale(1)';
+        }
+      });
   });
 }
