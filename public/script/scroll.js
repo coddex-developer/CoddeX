f0cec8b8cc556ef9a4940646c3083af4d8bf1b23
CoddeX <gabrielrodrigueslima22@gmail.com>
2025-01-14 09:53:57 -0300

    Ajustando centralização de certificados no scroll

--- public/script/scroll.js
+++ public/script/scroll.js
@@ -2,19 +2,24 @@ export default function scrollFunc() {
   const scrollContainer = document.querySelector('.certificateContainer');
   const scrollItems = document.querySelectorAll('.cardCertificate');
 
+  // Centraliza o primeiro item ao carregar a página
+  const centerItem = Math.floor(scrollItems.length / 2);
+  scrollContainer.scrollLeft = scrollItems[centerItem].offsetLeft - (scrollContainer.offsetWidth / 2) + (scrollItems[centerItem].offsetWidth / 2);
+
+  // Evento de scroll
   scrollContainer.addEventListener('scroll', () => {
     let center = scrollContainer.scrollLeft + scrollContainer.offsetWidth / 2;
 
-      scrollItems.forEach(item => {
-        const itemLeft = item.offsetLeft;
-        const itemRight = itemLeft + item.offsetWidth;
+    scrollItems.forEach(item => {
+      const itemLeft = item.offsetLeft;
+      const itemRight = itemLeft + item.offsetWidth;
 
-        if (center >= itemLeft && center <= itemRight) {
-          item.style.transform = 'scale(1.1)';
-          item.style.transition = 'transform 0.2s ease';
-        } else {
-          item.style.transform = 'scale(1)';
-        }
-      });
+      if (center >= itemLeft && center <= itemRight) {
+        item.style.transform = 'scale(1.1)';
+        item.style.transition = 'transform 0.2s ease';
+      } else {
+        item.style.transform = 'scale(1)';
+      }
+    });
   });
 }
