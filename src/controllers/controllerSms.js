414f61a925421b4761cd78b048b7f5966cdab4c4
CoddeX <gabrielrodrigueslima22@gmail.com>
2025-02-17 23:11:46 -0300

    Ajustes na responsividade do blog

--- src/controllers/controllerSms.js
+++ src/controllers/controllerSms.js
@@ -1,11 +1,36 @@
 const UUID = require('uuid').v4;
 const { DateTime } = require('luxon');
 const MessageDB = require("../db/messageDB")
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "seuemail@gmail.com",
+    pass: "suasenha", // Use uma senha de aplicativo se necessário
+  },
+});
 
 module.exports = {
 
   submitMessage: async (req, res) => {
     const { email, phone, name, messenger } = req.body;
+
+    const mailOptions = {
+      from: "seuemail@gmail.com",
+      to: "destinatario@gmail.com",
+      subject: "Confirmação de Mensalidade",
+      html: `
+    <div style="text-align: center; font-family: Arial, sans-serif;">
+      <img src="https://seusite.com/minha-logo.png" alt="Logo" width="150">
+      <h2>Confirmação de Mensalidade</h2>
+      <p>Recebemos sua mensagem! Sua mensalidade está confirmada.</p>
+      <a href="https://seusite.com/confirmacao" style="background-color: #28a745; color: white; padding: 10px 20px; text-decoration: none; border-radius: 5px; display: inline-block; font-size: 16px;">
+        Confirmar Pagamento
+      </a>
+    </div>
+  `,
+    };
 
     const brTime = DateTime.now().setZone('America/Sao_Paulo');
 
